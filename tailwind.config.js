9fc2740e566c5b859db65a47c2c24c098e013f02
Vinay Bhaskar <vinaybhaskar@Vinays-MacBook-Air.local>
2025-02-25 15:35:24 -0600

    Fixed Tailwind styles and layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,11 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",  // Ensure Tailwind scans all files in 'app' directory
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
