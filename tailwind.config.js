c20130bf9b50cf3f7a652bb147950fb6e7319a0a
Vinay Bhaskar <vinaybhaskar@Vinays-MacBook-Air.local>
2025-02-25 17:01:18 -0600

    Tested portfolio locally, ready for deployment

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",  // Ensure Tailwind scans all files in 'app' directory
+    "./app/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
